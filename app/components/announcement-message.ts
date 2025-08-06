235dfc0d45161e6b3089366204cd46affa77c30c
Ciaran McAleer <72049678+CiaranMcAleer@users.noreply.github.com>
2025-08-06 15:35:54 +0100

    Update announcements

--- app/components/announcement-message.ts
+++ app/components/announcement-message.ts
@@ -1,9 +1,9 @@
 //TODO when adding announcements, update this file with the new messages.
 
 const ANNOUNCEMENT_MESSAGES = [
-  "Welcome to TikTok Multiviewer v0.2.1 Check out the new features and updates in the dashboard. Feedback is welcome!",
+  "Welcome to TikTok Multiviewer v0.2.2 Check out the new features and updates in the dashboard. Feedback is welcome!",
   "If you encounter any issues or have any requests for features, please report them to me on the GitHub repository https://github.com/CiaranMcAleer/Tiktok-Multiviewer/issues",
-  "In the v0.1.3 update we have improved mobile UX so components should scale better on smaller screens, if you encounter any scaling issues, plese submit an issue"
+  "In the 0.2.2 update, the TWNI scraping and camera embed functionality has been improved. This should resolve any mismatches.",
 ];
 
 export default ANNOUNCEMENT_MESSAGES;
